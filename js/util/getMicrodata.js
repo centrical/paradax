3040c5874cfc836dfb3a181b21518aca71fc7f98
Sander Nagtegaal <sander@peecho.com>
2013-10-26 15:53:31 +0200

    Added noscript element
    According to Google's best practices, you should all content that you
    hide to prevent either reflow or flash of unstyled content to a
    noscript element.

--- js/util/getMicrodata.js
+++ js/util/getMicrodata.js
@@ -1,16 +1,1 @@
-define(["jquery", "microdata", "microdataJson"], function() {
-	'use strict';
-	return function getMicrodata(selector) {
-		var jsonHtml = $.microdata.json(selector, function(o) {
-			return o;
-		});
-		// adding descriptive property for itemType
-		$.each(jsonHtml.items, function() {
-			var typeUrl = this.type[0];
-			var typeTitle = typeUrl.substr(typeUrl.lastIndexOf('/') + 1);
-			this[typeTitle] = true;
-		});
-		//console.log(jsonHtml);
-		return jsonHtml
-	};
-});
+define(["jquery","microdata","microdataJson"],function(){return function(t){var n=$.microdata.json(t,function(e){return e});return $.each(n.items,function(){var e=this.type[0],t=e.substr(e.lastIndexOf("/")+1);this[t]=!0}),n}});
